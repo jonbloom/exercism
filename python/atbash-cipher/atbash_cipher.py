3015727fdc664d93e336e2b86c9cd4148b3e8f0c
Jon Bloom <stash@jon.tw>
2015-04-20 14:18:39 -0400

    fix bugs

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -2,14 +2,9 @@ from string import ascii_lowercase, digits, maketrans, translate
 from re import findall
 
 def encode(string):
-	string = ''.join(findall('[A-z0-9]',string.lower()))
+	string = ''.join(findall('[a-z0-9]',string.lower()))
 	trans_table = maketrans(ascii_lowercase+digits, ''.join(reversed(ascii_lowercase))+digits)
 	string = translate(string, trans_table)
-	encoded = ''
-	for i in xrange(len(string)):
-		if i > 1 and i % 5 == 0:
-			encoded += ' '
-		encoded += string[i]
-	return encoded
+	return ' '.join(findall(".{1,5}", string))
 def decode(string):
 	return encode(string).replace(' ','')
