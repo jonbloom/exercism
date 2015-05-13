ddd4d3dd8e89107595e7f279749ad3ea6ff449f4
Jon Bloom <stash@jon.tw>
2015-05-13 16:28:26 -0400

    hello, world! and also grade-school

--- python/kindergarten-garden/garden.py
+++ python/kindergarten-garden/garden.py
@@ -3,32 +3,18 @@ class Garden(object):
 	_default_students = ['Alice', 'Bob', 'Charlie', 'David',
 				'Eve', 'Fred', 'Ginny', 'Harriet',
 				'Ileana', 'Joseph', 'Kincaid', 'Larry']
-	_row1 = None
-	_row2 = None
-	_students = None
+	_plants = {'G': 'Grass', 'C': 'Clover', 'R': 'Radishes', 'V': 'Violets'}
 
 	def __init__(self, diagram, students=_default_students):
 		self._row1 = diagram.split('\n')[0]
 		self._row2 = diagram.split('\n')[1]
 		self._students = sorted(students)
-
-	def _get_long_name(self,initial):
-		if initial == 'G':
-			return 'Grass'
-		elif initial == 'C':
-			return 'Clover'
-		elif initial == 'R':
-			return 'Radishes'
-		elif initial == 'V':
-			return 'Violets'
-		else:
-			return None
 			
 	def plants(self, student):
 		rtn = list()
-		index = self._students.index(student)*2
-		for plant in self._row1[index:index+2]:
-			rtn.append(self._get_long_name(plant))
-		for plant in self._row2[index:index+2]:
-			rtn.append(self._get_long_name(plant))
+		i = self._students.index(student)*2
+		for plant in self._row1[i:i+2]:
+			rtn.append(self._plants[plant])
+		for plant in self._row2[i:i+2]:
+			rtn.append(self._plants[plant])
 		return rtn
